d675746ffc7ec2a1e426b7fc5b9cf6f05c25b968
yanjk3 <48338375+yanjk3@users.noreply.github.com>
2019-12-18 11:31:03 +0800

    Update train.py

--- 神经翻译/train.py
+++ 神经翻译/train.py
@@ -8,8 +8,8 @@ Model is in model.py.
 Firstly, read in training data, training target and their dict.
 Secondly, build the network, pay attention to the vocabulary size of encoder and decoder.
 Thirdly, divided data set into some batches.
-    For each batches, padding training data such that its length equal to the longest sequence of this batch.
-    What's more, padding training target until its length = max_length, which is a hyper parameter.
+    For each batches, padding training data and label,
+    such that its length equal to the longest sequence of this batch.
 Fourthly, throw the batch training data into Encoder, and get the output and the last hidden state.
     In this part, because the LSTM is bidirectional, the output size of it is (seq_len, bs, 2*hidden_dim),
     It show be reshaped to (seq_len, bs, hidden_dim), the way I choose is to add the forward output and
@@ -23,7 +23,8 @@ Fifthly, init the initial hidden state of Decoder by the last hidden state of En
     For each epoch, note down the average loss.
 Lastly, save the total model as a 'pkl' file and show the variation of loss.
 **********
-What you should focus on is that the dimension of input is not batch first,
+What you should focus on is:
+The dimension of input is not batch first,
 so I transpose the input before throw it into Encoder.
 What's more, the learning rate should not be too large,
 otherwise, it will make the loss oscillate near the minimum.
@@ -31,13 +32,14 @@ otherwise, it will make the loss oscillate near the minimum.
 Hyper parameters:
     learning_rate = 1e-4
     decoder_learning_rate = 5e-4
-    batch_size = 10
-    epochs = 200
-    max_length = 100
+    batch_size = 100
+    epochs = 1000
     teaching_force_ratio = 0.5
+
 Optimizer:
     Encoder: SGD
     Decoder: SGD
+
 Loss function:
     Cross Entropy
 """
@@ -64,6 +66,7 @@ def get_data(filename, length=8000):
         data.append(line)
         if len(data) == length:
             break
+    f.close()
     return data
 
 def get_num2word(filename):
@@ -77,37 +80,28 @@ def get_num2word(filename):
     for line in f:
         line = line.strip().split()
         data.append(line[1])
+    f.close()
     return data
 
-def padding_data(batch_data, num2word):
+def padding(batch_data, num2word, is_data):
     """
-    Padding training data until its length equal to the longest one in this batch
+    Padding data until its length equal to the longest one in this batch
     :param batch_data: a batch of data
     :param num2word: list, the dict number to word
+    :param is_data: True for training data, False for training label
     :return: np.array, the data after padding
     """
+    if is_data:
+        pad = num2word.index('<PAD>')
+    else:
+        pad = num2word.index('<EOS>')
     new_batch_data = list()
     length = 0
     for data in batch_data:
         length = len(data) if len(data) > length else length
     for data in batch_data:
         if len(data) < length:
-            data = data + [num2word.index('<PAD>')]*(length-len(data))
-        new_batch_data.append(data)
-    return np.array(new_batch_data, dtype='int64')
-
-def padding_label(batch_data, num2word, length):
-    """
-    Padding training target until its length = the max length
-    :param batch_data: a batch of data
-    :param num2word: list, the dict number to word
-    :param length: the max length of target
-    :return: np.array, the data after padding
-    """
-    new_batch_data = list()
-    for data in batch_data:
-        if len(data) < length:
-            data = data + [num2word.index('<EOS>')]*(length-len(data))
+            data = data + [pad]*(length-len(data))
         new_batch_data.append(data)
     return np.array(new_batch_data, dtype='int64')
 
@@ -121,14 +115,13 @@ if __name__=="__main__":
     # hyper parameter
     learning_rate = 1e-4
     decoder_learning_rate = 5e-4
-    batch_size = 10
-    epochs = 20
-    max_length = 100
+    batch_size = 100
+    epochs = 1000
     teaching_force_ratio = 0.5
 
     # generate NN
-    encoder = Encoder(len(source_num2word), 200, 500).cuda()
-    decoder = Decoder(len(target_num2word), 200, 500).cuda()
+    encoder = Encoder(len(source_num2word), 600, 200).cuda()
+    decoder = Decoder(len(target_num2word), 600, 200).cuda()
     if encoder.hidden_dim != decoder.hidden_dim:
         raise RuntimeError('Encoder and Decoder should have the same hidden dimension!')
 
@@ -162,9 +155,10 @@ if __name__=="__main__":
                 train_label = training_label[i*batch_size:(i + 1)*batch_size]
 
             # padding
-            train_data = padding_data(train_data, source_num2word)
-            train_label = padding_label(train_label, target_num2word, max_length)
+            train_data = padding(train_data, source_num2word, True)
+            train_label = padding(train_label, target_num2word, False)
 
+            max_length = train_label.shape[1]
             # transpose, (bs, seq_len) -> (seq_len, bs) because batch_first is false
             train_data = torch.LongTensor(train_data).transpose(0, 1).cuda()
             train_label = torch.LongTensor(train_label).transpose(0, 1).cuda()
@@ -193,7 +187,7 @@ if __name__=="__main__":
                         decoder_out, decoder_hidden = decoder(time_step_input, decoder_hidden, encoder_out)
                 # for each time step, calculate loss
                 loss += criterion(decoder_out.squeeze(), train_label[time_step])
-
+            print(loss/((i+1)*batch_size))
             # backward and update parameters
             encoder_optimizer.zero_grad()
             decoder_optimizer.zero_grad()
@@ -221,4 +215,4 @@ if __name__=="__main__":
     plt.xlabel('epoch')
     plt.ylabel('loss')
     plt.legend()
-    plt.show()
+    plt.savefig('loss.png', format='png')
